c6fbd48ac2f6a5b8618cb00a9773c31aab4b5312
Donovan Cartier <donovan.cartier0@gmail.com>
2021-11-26 17:09:22 +0100

    TOG behavior

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var islandNameText = document.getElementById('islandName');
 var islandCoordsText = document.getElementById('islandCoords');
 var islands = document.querySelectorAll(".island");
 
-var items = document.querySelectorAll(".itemIcon")
-
+var items = document.querySelectorAll(".itemIcon");
+var pearls = [...document.querySelectorAll(".pearl")];
 
 islands.forEach(element => {
     element.addEventListener("mouseover", function(){
@@ -18,5 +18,23 @@ islands.forEach(element => {
 items.forEach(element => {
     element.addEventListener("click", function(){
         this.classList.toggle("itemSelected");
+        if(pearls.every(checkActiveState) === true){
+            appearTowerOfGods();
+        }
+            else{
+                disappearTowerOfGods();
+            };
     })
 });
+
+function checkActiveState(el){
+    return el.classList.contains("itemSelected");
+}
+
+function appearTowerOfGods(){
+    document.getElementById('tog').style.opacity = "100";
+}
+
+function disappearTowerOfGods(){
+    document.getElementById('tog').style.opacity = "0" 
+}
